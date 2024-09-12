b12a70fcda9bbd780a205f6618753a95672b6076
DanicaMarpa <marpadanicajoyp@gmail.com>
2024-09-12 14:19:14 +0800

    increment and decrement counter

---
+++ vue-project-demo/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <p style="font-size: 30px;"> Count: {{ count }}</p>
+  <button v-on:click="increment" style="background-color: cyan; font-size: 30px; margin-left: 20px">+</button>
+  <button v-on:click="decrement" style="background-color: cyan; font-size: 30px;">-</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+let count = ref(0);
+
+const increment = () => count.value++;
+const decrement = () => count.value--;
+</script>
